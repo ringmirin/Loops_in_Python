7628a7b389fb9dd819619867f0d9422961c4b94c
ringmirin <ringmirin21@navgurukul.org>
2021-12-05 00:00:48 +0530

    first commit

---
+++ fibonacci_number.py
@@ -0,0 +1,19 @@
+# Make an algorithm that prints the first 100 numbers of 
+# the fibonacci series.
+# Fibonacci series is shown here, 0, 1, 1, 2, 3, 5, 8, 13
+
+
+    
+# a=0
+# b=1
+# c=int(input("enter the terms"))
+# if c<=0:
+#    print(a)
+# else:
+#    print(a,b,end=" ")
+#    for x in range(c):
+#       next=a+b
+#       print(next,end=" ")
+#       a=b
+#       b=next
+   
---
+++ floyds_triangle.py
@@ -0,0 +1,57 @@
+##floyds triangle## 
+ 
+# n=int(input("enter the number"))
+# num=1
+# for row in range(1,n+1):
+#    for col in range(1,row+1):
+#       print(num,end=" ")
+#       num+=1
+#    print()
+      
+      
+  ##pyramid    
+# num=int(input("enter the number"))
+
+# for x in range(num+1):
+#    print(" "*(num-x)+"* "*x)
+# for x in range(num-1,0,-1):
+#    print(" "*(num-x)+"* "*x)
+   
+   
+# n=int(input("enter the number"))
+# for row in range(n):
+#    for col in range(row+1):
+#       print(col+1,end=" ")
+#    print()
+   
+# n=int(input("enter the number"))
+# for row in range(n):
+#    for col in range(row,-1,-1):
+#       print(col+1,end=" ")
+#    print()
+
+
+# n=int(input("enter the number"))
+# num=1
+# for row in range(n):
+#    for col in range(row+1):
+#       print(num,end=" ")
+#       num+=1
+#    print()
+   
+
+# n=int(input("enter the number"))
+# for row in range(n):
+#    for col in range(row+1):
+#       print(col+1,end=" ")
+#    print()
+   
+   
+      
+# n=input("enter the string")
+
+# n=int(input("enter the number"))
+# for row in range(n,0,-1):
+#    for col in range(1,row+1):
+#       print(row,end=" ")
+#    print()
