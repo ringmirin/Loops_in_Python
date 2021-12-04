28d0de11491045f4bdc2d1030b5601f0ed5415e6
ringmirin <ringmirin21@navgurukul.org>
2021-12-05 00:28:08 +0530

    y first commit

---
+++ prime_number.py
@@ -0,0 +1,10 @@
+####prime number {2,3,5,7,11,17......}
+  
+  
+# for num in range(1,100+1):
+#    if num>1:
+#       for i in range(2,num):
+#          if (num%i)==0:
+#             break
+#       else:
+#          print(num)
